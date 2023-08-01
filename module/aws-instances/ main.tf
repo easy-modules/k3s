56d8359ae9100faaca80c6e9032694955fa706f0
ArthurMaverick <arthuracs18@gmail.com>
2023-08-01 10:25:56 -0300

    feat: Add K3S

--- module/aws-instances/ main.tf
+++ module/aws-instances/ main.tf
@@ -1,14 +1,44 @@
+data "aws_availability_zones" "available" {}
+
 locals {
-    default_tags = {
-        Environment = var.environment == null ? "" : var.environment
-        Owner       = var.owner == null ? "" : var.owner
-        Project     = var.project == null ? "" : var.project
-        CreatedBy   = "Terraform"
-    }
+  azs = data.aws_availability_zones.available.names
+  default_tags = {
+      CreatedBy   = "Terraform"
+  }
 }
 #===============================================================================
-# AWS EC2
+# AWS EC2 EBS
 #===============================================================================
+resource "aws_ebs_volume" "ebs" {
+  availability_zone = var.availability_zone == null ? var.availability_zone : local.azs[0] ## us-west-2a
+  encrypted         = var.encrypted
+  type              = var.type
+  size              = var.size
+  tags              = merge(
+    local.default_tags,
+    { Name = format("%s-ebs-%s", var.common_prefix, var.environment) }
+  )
+}
+#===============================================================================
+# AWS EC2 KEY PAIR
+#===============================================================================
+resource "aws_key_pair" "my_ssh_public_key" {
+  key_name   = "${var.common_prefix}-ssh-pubkey-${var.environment}"
+  public_key = file(var.path_to_public_key)
+
+  tags = merge(
+    local.default_tags,
+    {
+      Name = lower("${var.common_prefix}-ssh-pubkey-${var.environment}")
+    }
+  )
+}
+#===============================================================================
+# AWS EC2 INSTANCE
+#===============================================================================
+locals {
+  user_data = fileexists(var.user_data_path) ? file(var.user_data_path) : file("${path.module}/scripts/user_data.sh")
+}
 data "aws_ami" "amazon" {
   most_recent = true
   owners      = ["amazon"]
@@ -22,38 +52,21 @@ data "aws_ami" "amazon" {
   }
 
 }
-resource "aws_key_pair" "my_ssh_public_key" {
-  key_name   = "${var.common_prefix}-ssh-pubkey-${var.environment}"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
-
-  tags = merge(
-    local.default_tags,
-    {
-      "Name" = lower("${var.common_prefix}-ssh-pubkey-${var.environment}")
-    }
-  )
-}
-resource "aws_ebs_volume" "ebs" {
-  availability_zone = var.availability_zone ## us-west-2a
-  encrypted         = true
-  type              = "gp2"
-  size              = 8
-  tags              = merge(
-    local.default_tags,
-    { Name = format("%s-ebs-%s", ) }
-  )
-}
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.amazon.id
-  instance_type = "t4g.nano"
-  user_data     = ""
-  tags = {
-    Name = "k3s-server"
-  }
+  instance_type = var.instance_type
+  user_data     = local.user_data
+  key_name      = aws_key_pair.my_ssh_public_key.key_name
+  tags = merge(
+    local.default_tags,
+    { Name = lower(format("%s-ec2-%s", var.common_prefix, var.environment)) }
+  )
 }
+#===============================================================================
+# AWS EC2 EBS ATTACHMENT
+#===============================================================================
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.ebs.id
   instance_id = aws_instance.ec2.id
-
 }
--- module/aws-instances/variables.tf
+++ module/aws-instances/variables.tf
@@ -1,35 +1,59 @@
-
 #===============================================================================
 # AWS EC2 KEY PAIR
 #===============================================================================
 variable "common_prefix" {
-  type    = string
+  type        = string
   description = "set commom prefix"
-  default = "k3s"
+  default     = "k3s"
 }
-
 variable "environment" {
-  type = string
+  type        = string
   description = "set up environment"
-  default = "dev"
+  default     = "dev"
 }
-
 variable "global_tags" {
-  type = map(string)
+  type        = map(string)
   description = "set global tags"
-  default = {}
+  default     = {}
 }
-
-variable "PATH_TO_PUBLIC_KEY" {
-  type = string
+variable "path_to_public_key" {
+  type        = string
   description = "set path of public key"
 }
-
+#===============================================================================
+# AWS EC2 STORAGE
+#===============================================================================
+variable "availability_zone" {
+  type        = string
+  description = "set availability zone"
+  default     = null
+}
+variable "encrypted" {
+  type        = bool
+  description = "set encrypted"
+  default     = true
+}
+variable "type" {
+  type        = string
+  description = "set type"
+  default     = "gp2"
+}
+variable "size" {
+  type        = number
+  description = "set size"
+  default     = 8
+}
 #===============================================================================
 # AWS EC2 INSTANCE
 #===============================================================================
-variable "availability_zone" {
-  type = string
-  description = "set availability zone"
-  default = null
+variable "instance_type" {
+  type        = string
+  description = "set instance type"
+  default     = "t4g.nano"
 }
+variable "user_data_path" {
+  type        = string
+  description = "set user data"
+  default     = null
+}
+
